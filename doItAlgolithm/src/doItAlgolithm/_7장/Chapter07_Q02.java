57be6455c5e5b8d831b0121c0832155713569ffd
Daniel5629 <apxls34@gmail.com>
2020-08-30 19:31:14 +0900

    7장

---
+++ doItAlgolithm/src/doItAlgolithm/_7장/Chapter07_Q02.java
@@ -0,0 +1,127 @@
+package doItAlgolithm._7장;
+
+public class Chapter07_Q02 {
+	private int max; 
+	private int num; 
+	private int[] set;
+
+	public Chapter07_Q02(int capacity) {
+		num = 0;
+		max = capacity;
+		try {
+			set = new int[max]; 
+		} catch (OutOfMemoryError e) { 
+			max = 0;
+		}
+	}
+
+	public int capacity() {
+		return max;
+	}
+
+	public int size() {
+		return num;
+	}
+
+	public int indexOf(int n) {
+		for (int i = 0; i < num; i++)
+			if (set[i] == n)
+				return i; 
+		return -1; 
+	}
+
+	public boolean contains(int n) {
+		return (indexOf(n) != -1) ? true : false;
+	}
+
+	public boolean add(int n) {
+		if (num >= max || contains(n) == true) 
+			return false;
+		else {
+			set[num++] = n; 
+			return true;
+		}
+	}
+
+	public boolean remove(int n) {
+		int idx; 
+
+		if (num <= 0 || (idx = indexOf(n)) == -1) 
+			return false;
+		else {
+			set[idx] = set[--num];
+			return true;
+		}
+	}
+
+	public void copyTo(Chapter07_Q02 s) {
+		int n = (s.max < num) ? s.max : num; 
+		for (int i = 0; i < n; i++)
+			s.set[i] = set[i];
+		s.num = n;
+	}
+
+	public void copyFrom(Chapter07_Q02 s) {
+		int n = (max < s.num) ? max : s.num; 
+		for (int i = 0; i < n; i++)
+			set[i] = s.set[i];
+		num = n;
+	}
+
+	public boolean equalTo(Chapter07_Q02 s) {
+		if (num != s.num)
+			return false;
+
+		for (int i = 0; i < num; i++) {
+			int j = 0;
+			for (; j < s.num; j++)
+				if (set[i] == s.set[j])
+					break;
+			if (j == s.num)
+				return false;
+		}
+		return true;
+	}
+
+	public void unionOf(Chapter07_Q02 s1, Chapter07_Q02 s2) {
+		copyFrom(s1); 
+		for (int i = 0; i < s2.num; i++)
+			add(s2.set[i]);
+	}
+
+	public String toString() {
+		StringBuffer temp = new StringBuffer("{ ");
+		for (int i = 0; i < num; i++)
+			temp.append(set[i] + " ");
+		temp.append("}");
+		return temp.toString();
+	}
+
+	public boolean add(Chapter07_Q02 s) {
+		boolean flag = false;
+		for (int i = 0; i < num; i++)
+			if (add(set[i]) == true)
+				flag = true;
+		return flag;
+	}
+
+	public boolean retain(Chapter07_Q02 s) {
+		boolean flag = false;
+		for (int i = 0; i < num; i++)
+			if (s.contains(set[i]) == false) {
+				remove(set[i]);
+				flag = true;
+			}
+		return flag;
+	}
+
+	public boolean remove(Chapter07_Q02 s) {
+		boolean flag = false;
+		for (int i = 0; i < num; i++)
+			if (s.contains(set[i]) == true) {
+				remove(set[i]);
+				flag = true;
+			}
+		return flag;
+	}
+}
